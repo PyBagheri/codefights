85fac36a570f051a23386eb8b2b7e6f1987df3d1
MohammadHossein Bagheri <py.persian@gmail.com>
2024-06-01 22:34:44 +0330

    add pages view

--- pages/views.py
+++ pages/views.py
@@ -1,3 +1,16 @@
+from typing import Any
 from django.shortcuts import render
+from django.views.generic import TemplateView
 
-# Create your views here.
+
+class Handler404(TemplateView):
+    template_name = '404.html'
+
+
+class Handler500(TemplateView):
+    template_name = '500.html'
+
+
+class GuideView(TemplateView):
+    template_name = 'guide.html'
+    
