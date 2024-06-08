cfbc5d5f457d614a9e01a85a2f309ddee3df4381
MohammadHossein Bagheri <py.persian@gmail.com>
2024-06-09 02:16:39 +0330

    bugfix: authentication backend didn't have the necessary kwarg 'username'

--- accounts/backends.py
+++ accounts/backends.py
@@ -5,12 +5,19 @@ from django.db import models
 
 
 class UsernameOrEmailBackend(BaseBackend):
-    def authenticate(self, request, username_or_email=None, password=None):
+    def authenticate(self, request, username=None, username_or_email=None, password=None):
+        if username is None and username_or_email is None:
+            raise ValueError("at least one of 'username' or 'username_or_email' must be given")
+        
+        if username_or_email is None:
+            username_or_email = username
+        
         user = User.active.filter(
             models.Q(username=username_or_email) | models.Q(email=username_or_email)
         ).first()
         
         if not user:
+            User().set_password(password)  # see ModelBackend's comments.
             return None
         
         if user.check_password(password):
