fc14795232cc176993366610b6850e55cee21c2a
MohammadHossein Bagheri <py.persian@gmail.com>
2024-06-09 02:17:48 +0330

    bugfix: use cursor for each raw SQL execution
    Storing a single cursor would make the program unusable in case there
    was a disconnection. This would happen when we stop the docker containers
    without removing them, and trying to restart them again (which would
    try to use the previous state of the container, but it wouldn't work
    since the database connection is lost [not totally sure]).

--- utils/postgres.py
+++ utils/postgres.py
@@ -1,8 +1,5 @@
 from django.db import connection
 
-# We'll keep only one cursor.
-cursor = connection.cursor()
-
 
 def pg_try_advisory_xact_lock(i1: int, i2: int = None):
     if i2 is None:
@@ -12,9 +9,9 @@ def pg_try_advisory_xact_lock(i1: int, i2: int = None):
         query = "SELECT pg_try_advisory_xact_lock( %s , %s )"
         params = [i1, i2]
 
-    cursor.execute(query, params)
-    
-    return cursor.fetchone()[0]
+    with connection.cursor() as cursor:
+        cursor.execute(query, params)
+        return cursor.fetchone()[0]
         
 
 def pg_advisory_xact_lock(i1: int, i2: int = None):
@@ -25,7 +22,5 @@ def pg_advisory_xact_lock(i1: int, i2: int = None):
         query = "SELECT pg_advisory_xact_lock( %s , %s )"
         params = [i1, i2]
 
-    cursor.execute(query, params)
-    
-    cursor.fetchone()  # flush the cursor
-
+    with connection.cursor() as cursor:
+        cursor.execute(query, params)
